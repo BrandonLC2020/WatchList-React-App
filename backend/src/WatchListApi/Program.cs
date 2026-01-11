09a6895fb0a047f913c75474d3a2a49ee21213c6
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2026-01-11 12:09:14 -0500

    configuring firebase

--- backend/src/WatchListApi/Program.cs
+++ backend/src/WatchListApi/Program.cs
@@ -3,6 +3,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+// 1. Read configuration
+var projectId = builder.Configuration["Firebase:ProjectId"];
+var credentialPath = builder.Configuration["Firebase:CredentialPath"];
+
+// 2. Set the environment variable for authentication (if using a file path)
+if (!string.IsNullOrEmpty(credentialPath))
+{
+    Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialPath);
+}
+
+// 3. Register FirestoreDb as a Singleton
+builder.Services.AddSingleton<FirestoreDb>(provider => 
+    FirestoreDb.Create(projectId));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
